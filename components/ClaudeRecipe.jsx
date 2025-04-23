525f82b087f338f83c2ac0b3b3a24176ea62855d
Alto0327 <aj.silvaf2003@gmail.com>
2025-04-23 10:56:48 -0400

    Made the Markdown More Readable

--- components/ClaudeRecipe.jsx
+++ components/ClaudeRecipe.jsx
@@ -1,7 +1,13 @@
+import ReactMarkdown from 'react-markdown'
+
 export default function ClaudeRecipe({recipe}) {
+  
+  const markdown = recipe
+
   return (
-   <section>
-      <p>{recipe}</p>
+    <section className="suggested-recipe-container" aria-live="polite">
+      <h2>Chef IR Suggests:</h2>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
     </section>
   );
 }
